69d078106adf43f4009d5e47ce18831ac274e1ee
Mariya Podchishchaeva <fznamznon23@gmail.com>
2017-12-27 14:50:44 +0300

    Small changes in rv basis

--- develop/model.js
+++ develop/model.js
@@ -713,8 +713,8 @@ function addMeshesControl() {
     lv.getBasisFromFile("2_s.obj", "lv_f.obj", null, null, lvAVG);
     mitral.getBasisFromFile("2_s.obj", "3_f.obj",
         null, null, lvAVG);
-	rv.getBasisFromFile("4_s.obj", "5_f.obj", null, null, rvAVG);
-	tricuspid.getBasisFromFile("4_s.obj", "tricusp_finish_new.obj",
+	rv.getBasisFromFile("rv_start_new.obj", "5_f.obj", null, null, rvAVG);
+	tricuspid.getBasisFromFile("rv_start_new.obj", "tricusp_finish_new.obj",
         null, null, rvAVG);
 
 }
